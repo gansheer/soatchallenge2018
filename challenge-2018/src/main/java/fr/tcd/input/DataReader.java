66f5511cd1d47cbe0d0bf69e731d2796faea5a0a
Patrick Allain <patrick.allain@soat.fr>
2018-02-22 19:52:15 +0100

    Add files

--- challenge-2018/src/main/java/fr/tcd/input/DataReader.java
+++ challenge-2018/src/main/java/fr/tcd/input/DataReader.java
@@ -1,11 +1,21 @@
 package fr.tcd.input;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * @author patrick.allain@soat.fr - 2/22/18.
  */
 public class DataReader {
 
     public static Object load(final String dataset) {
-        return null;
+//        final Scanner in = new Scanner(ClassLoader.getSystemResourceAsStream(dataset));
+        try {
+            Files.readAllLines(Paths.get("src/main/java/", dataset));
+            return null;
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load file");
+        }
     }
 }
