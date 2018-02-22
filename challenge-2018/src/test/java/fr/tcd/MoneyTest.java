4739b8f2611b16baf3a2f889513df4d788071f8c
Patrick Allain <patrick.allain@soat.fr>
2018-02-22 20:47:51 +0100

    Add files

--- challenge-2018/src/test/java/fr/tcd/MoneyTest.java
+++ challenge-2018/src/test/java/fr/tcd/MoneyTest.java
@@ -4,6 +4,7 @@ import fr.tcd.input.DataReader;
 import fr.tcd.input.Info;
 import fr.tcd.input.Input;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 /**
@@ -15,13 +16,21 @@ public class MoneyTest {
     public void toto() {
         // GIVEN
         final Input input = DataReader.load("crypto-input.txt");
-        final Info info = input.infos.get(1);
+        // WHEN
+        // THEN
+    }
+
+    @Test
+    public void interest() {
+        // GIVEN
+        final Input input = DataReader.load("crypto-input.txt");
+        final Info info = input.infos.get(2);
 
         // WHEN
         final long derivate = new Money(info).interest();
 
         // THEN
-//        Assertions.assertThat(derivate).as("Money %s ", info.nom).isGreaterThan(0L);
+        Assertions.assertThat(derivate).as("Money %s ", info.nom).isGreaterThan(0L);
 
     }
 
