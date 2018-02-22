c2df7560d156fe3dfe168b1a5e5daaa214c7acf5
gansheer <gaelle.fournier.work@gmail.com>
2018-02-22 19:52:16 +0100

    input

---
+++ challenge-2018/src/main/java/fr/tcd/input/Info.java
@@ -0,0 +1,9 @@
+package fr.tcd.input;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Info {
+	public String nom;
+	public List<Integer> valeurs = new ArrayList<>();
+}
---
+++ challenge-2018/src/main/java/fr/tcd/input/Input.java
@@ -0,0 +1,13 @@
+package fr.tcd.input;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Input {
+	public int c;
+	public int h;
+	public int p;
+	public int w;
+	public List<Info> infos = new ArrayList<>();
+}
+
