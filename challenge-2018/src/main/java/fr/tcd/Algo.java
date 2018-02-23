118fe320889e9ba979ae41adc05212dff44851e3
gansheer <gaelle.fournier.work@gmail.com>
2018-02-23 14:00:11 +0100

    less dumb idea

--- challenge-2018/src/main/java/fr/tcd/Algo.java
+++ challenge-2018/src/main/java/fr/tcd/Algo.java
@@ -1,9 +1,9 @@
 package fr.tcd;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 import fr.tcd.input.Info;
 import fr.tcd.input.Input;
@@ -15,16 +15,36 @@ import fr.tcd.result.ResultLine;
 public class Algo {
 
 	public static List<ResultLine> run(final Input data) {
-		Optional<Info> info = data.infos.stream().findFirst();
-		Optional<Integer> montantInvesti = info.get().valeurs.stream().filter(valeur -> valeur < data.c)
-				.sorted(Comparator.reverseOrder()).findFirst();
-		ResultLine oneResult = new ResultLine();
-		oneResult.nom = info.get().nom;
-		oneResult.actions.add("" + montantInvesti.get());
-		for (int i = 0; i < data.p - 2; i++) {
-			oneResult.actions.add("0");
+
+		List<Info> infos = data.infos.stream().sorted(new Comparator<Info>() {
+			@Override
+			public int compare(Info o1, Info o2) {
+				return o1.valeurs.get(data.h - 1).compareTo(o2.valeurs.get(data.h - 1));
+			}
+
+		}).filter(info -> info.valeurs.get(data.h - 2)+4 < info.valeurs.get(data.h - 1)).collect(Collectors.toList());
+
+		List<ResultLine> results = new ArrayList<>();
+		int div = 2;
+		for (int jour = 0; jour < data.p - 1 - div; jour += 1) {
+			results.add(generate(data, infos.get(jour), jour, div));
 		}
-		oneResult.actions.add("ALLOUT");
-		return Arrays.asList(oneResult);
+		return results;
+	}
+
+	public static ResultLine generate(Input data, Info info, int jour, int div) {
+		ResultLine oneResult = new ResultLine();
+		oneResult.nom = info.nom;
+		for (int i = 0; i < data.p; i++) {
+			if (i == jour) {
+				int montant = data.c/div;
+				oneResult.actions.add("" + montant);
+			} else if (i == jour + div) {
+				oneResult.actions.add("ALLOUT");
+			} else {
+				oneResult.actions.add("0");
+			}
+		}
+		return oneResult;
 	}
 }
